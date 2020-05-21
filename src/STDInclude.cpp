7a595f68236155f8d0a2d1b9aca9f443df097b2d
kayfu <65260802+kayfu@users.noreply.github.com>
2020-05-21 12:17:54 -0400

    AMD driver hint to use high-performance GPU

--- src/STDInclude.cpp
+++ src/STDInclude.cpp
@@ -74,6 +74,7 @@ extern "C"
 
 	// Enable 'High Performance Graphics'
 	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 };
 
 // Bridge d3d9
