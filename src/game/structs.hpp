aff2b641dd427188f18d765c38b6ab497c38e330
xoxor4d <45299104+xoxor4d@users.noreply.github.com>
2022-06-20 11:10:27 +0200

    add gameWorldSp pathnode dumping
    really specific use-case :p

--- src/game/structs.hpp
+++ src/game/structs.hpp
@@ -3210,6 +3210,160 @@ namespace game
 		FX_ELEM_TYPE_LAST_DRAWN = 0x7,
 	};
 
+	/* 871 */
+	struct pathlink_s
+	{
+		float fDist;
+		unsigned __int16 nodeNum;
+		char disconnectCount;
+		char negotiationLink;
+		char ubBadPlaceCount[4];
+	};
+
+	enum nodeType
+	{
+		NODE_BADNODE = 0x0,
+		NODE_PATHNODE = 0x1,
+		NODE_COVER_STAND = 0x2,
+		NODE_COVER_CROUCH = 0x3,
+		NODE_COVER_CROUCH_WINDOW = 0x4,
+		NODE_COVER_PRONE = 0x5,
+		NODE_COVER_RIGHT = 0x6,
+		NODE_COVER_LEFT = 0x7,
+		NODE_COVER_WIDE_RIGHT = 0x8,
+		NODE_COVER_WIDE_LEFT = 0x9,
+		NODE_CONCEALMENT_STAND = 0xA,
+		NODE_CONCEALMENT_CROUCH = 0xB,
+		NODE_CONCEALMENT_PRONE = 0xC,
+		NODE_REACQUIRE = 0xD,
+		NODE_BALCONY = 0xE,
+		NODE_SCRIPTED = 0xF,
+		NODE_NEGOTIATION_BEGIN = 0x10,
+		NODE_NEGOTIATION_END = 0x11,
+		NODE_TURRET = 0x12,
+		NODE_GUARD = 0x13,
+		NODE_NUMTYPES = 0x14,
+		NODE_DONTLINK = 0x14,
+	};
+
+	/* 872 */
+	struct pathnode_constant_t
+	{
+		nodeType type;
+		unsigned __int16 spawnflags;
+		unsigned __int16 targetname;
+		unsigned __int16 script_linkName;
+		unsigned __int16 script_noteworthy;
+		unsigned __int16 target;
+		unsigned __int16 animscript;
+		int animscriptfunc;
+		float vOrigin[3];
+		float fAngle;
+		float forward[2];
+		float fRadius;
+		float minUseDistSq;
+		__int16 wOverlapNode[2];
+		__int16 wChainId;
+		__int16 wChainDepth;
+		__int16 wChainParent;
+		unsigned __int16 totalLinkCount;
+		pathlink_s* Links;
+	};
+
+	/* 873 */
+	struct pathnode_dynamic_t
+	{
+		void* pOwner;
+		int iFreeTime;
+		int iValidTime[3];
+		int inPlayerLOSTime;
+		__int16 wLinkCount;
+		__int16 wOverlapCount;
+		__int16 turretEntNumber;
+		__int16 userCount;
+	};
+
+	struct pathnode_t;
+
+	/* 875 */
+	struct pathnode_transient_t
+	{
+		int iSearchFrame;
+		pathnode_t* pNextOpen;
+		pathnode_t* pPrevOpen;
+		pathnode_t* pParent;
+		float fCost;
+		float fHeuristic;
+		float costFactor;
+	};
+
+	/* 874 */
+	struct pathnode_t
+	{
+		pathnode_constant_t constant;
+		pathnode_dynamic_t dynamic;
+		pathnode_transient_t transient;
+	};
+
+	/* 876 */
+	struct pathbasenode_t
+	{
+		float vOrigin[3];
+		unsigned int type;
+	};
+
+	/* 878 */
+	struct pathnode_tree_nodes_t
+	{
+		int nodeCount;
+		unsigned __int16* nodes;
+	};
+
+	struct pathnode_tree_t;
+
+	/* 879 */
+	union pathnode_tree_info_t
+	{
+		pathnode_tree_t* child[2];
+		pathnode_tree_nodes_t s;
+	};
+
+	/* 877 */
+	struct pathnode_tree_t
+	{
+		int axis;
+		float dist;
+		pathnode_tree_info_t u;
+	};
+
+	/* 880 */
+	struct PathData
+	{
+		unsigned int nodeCount;
+		pathnode_t* nodes;
+		pathbasenode_t* basenodes;
+		unsigned int chainNodeCount;
+		unsigned __int16* chainNodeForNode;
+		unsigned __int16* nodeForChainNode;
+		int visBytes;
+		char* pathVis;
+		int nodeTreeCount;
+		pathnode_tree_t* nodeTree;
+	};
+
+	/* 881 */
+	struct GameWorldSp
+	{
+		const char* name;
+		PathData path;
+	};
+
+	/* 882 */
+	struct GameWorldMp
+	{
+		const char* name;
+	};
+
 	union XAssetHeader
 	{
 		void* data;
@@ -3226,7 +3380,7 @@ namespace game
 		// 			SndCurve *sndCurve;
 		clipMap_t* clipMap;
 		ComWorld* comWorld;
-		// 			GameWorldSp *gameWorldSp;
+		GameWorldSp *gameWorldSp;
 		// 			GameWorldMp *gameWorldMp;
 		MapEnts* mapEnts;
 		GfxWorld* gfxWorld;
